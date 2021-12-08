f08f339a8d2483915b7ba3d971749b80ce43e56e
Valeria Gutierrez <valeria.gutierrez.murillo@gmail.com>
2021-12-07 19:29:22 -0600

    version 3

--- js/sketch.js
+++ js/sketch.js
@@ -1,91 +1,99 @@
-var x=0;
-var y=0;
-var ix=3;
-var iy=2;
-var xx=1;
-var yy=1;
-var ixx=2;
-var iyy=3;
-var buenas=0,malas=0;
+let angles = [207.7,57.24,27.7,11.52,7.92,25.56,22.68];
+var x=230;
+var y=300;
 function setup() {
-  createCanvas(400, 400);
-  x=random(200,200);
-  y=random(200,200);
-  xx=random(200,200);
-  yy=random(200,200);
-  noStroke();
-  
-  
+  createCanvas(500, 400);
+  textFont("monaco");
+  //loadFont('/js/Digital.ttf', drawText);
+ colorMode(RGB,200,100,100);
+  col = color("yellow");
+  button = createButton('VALORES EXACTOS');
+  button.style('font-size', '15px');
+  button.style("font-family", "monaco");
+  button.style('background-color', col);
+  button.position(360, 470);
+  button.mousePressed(valores);
+  linea();
+  texto();
+}
+function cuadricula(){
+ var i, j;
+  for(i=0;i<=width;i+=50)
+    for(j=0;j<=height;j+=50){
+      line(i,0,i,height)
+      line(0,j,width,j)
+    }
 }
 
 function draw() {
-  background(220);
-  
-  fill(0,0,255);
-  ellipse(x,y, 50,50);
-  x+=ix;
-  y+=iy;
-  
-  fill(0,255,255);
-  ellipse(xx,yy, 50,50);
-  xx+=ixx;
-  yy+=iyy;
-  fill(0);
-  rect(0, mouseY, 10,100);
-  
-   
-  
- 
-  //Detectar colision.
-  if((y<=mouseY +100) && (y >= mouseY) && (x<=35))
-  {
-  console.log("xD");
-    buenas+=1;
-    ix=-ix;
+  noStroke();
+  let lastAngle = 0;
+  frameRate(0.7);
+  for (let i = 0; i < angles.length; i++) 
+  {     
+    fill(random(200),random(100),random(250));
     
-  }
-  
-  
-   if((yy<=mouseY +100) && (yy >= mouseY) && (xx<=35))
-  {
-  console.log("LOL");
-    buenas+=1;
-    ixx=-ixx;
-    
+    arc(
+      width / 2,
+      height / 2,
+      200,
+      200,
+      lastAngle,
+      lastAngle + radians(angles[i])
+    );
+    lastAngle += radians(angles[i]);
+
   }
 
-if(xx>=width-25 || xx<=25) 
+ 
+}
+
+function texto(font) {
+  fill('BLACK');
+  //textFont(font,21);
+  text('PROGRAMMING LANGUAGES',30, 40);
+  text("2021",230,380);
+  textSize(15);
+  text("MYSQL",170,350);
+  text("POSTGRESQL",30,127);
   
-  {
-  if(xx<=25)
-    
-   malas+=1;
-   ixx=-ixx;
-  console.log("LL");
-  }
-  if(yy>=height-25 || yy<=25)
-  {
-   iyy=-iyy;
-  }
-  
-  
-  textFont("Bodoni");
-  textSize(20);
-  text("Buenas: "+buenas,20,20 );
-  text("Malas:  "+malas,20,50 );
-  
-if(x>=width-25 || x<=25) 
-  
-  {
-  if(x<=25)
-    
-   malas+=1;
-   ix=-ix;
-    console.log("DD");
-  }
-  if(y>=height-25 || y<=25)
-  {
-   iy=-iy;
-  }
-  
+  text("EXCEL",180,70);
+  text("HTML5 & CS3",360,290);
+  text("PYTHON ",380,155);
+ 
+  text("C",350,127);
+  text("JAVA SCRIPT",320,75);
+ 
+}
+function linea()
+{
+  strokeWeight(2);
+  line(230,300,230,340);
+  line(230,340,220,340);
+  //
+  line(200,120,120,120);
+  //
+  line(250,65,250,100);
+  line(250,65,230,65);
+  //
+  line(360,190,340,190);
+  line(360,190,360,270);
+  //
+  line(340,150,370,150);
+  //
+  line(310,122,340,122);
+  //
+  line(295,122,295,70);
+  line(295,70,310,70);
+}
+
+function valores(){
+fill("black");
+text("57.7%",240,340);
+text("15.9%",80,165);  
+text("7.1%",395,200);   
+text("6.3%",360,310); 
+text("7.7%",70,90); 
+text("3.2%",380,80); 
+text("2.2%",450,135); 
 }
